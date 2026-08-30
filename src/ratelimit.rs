f3945ff9551bc69172b33836b0bab55b874be0e0
baalajimaestro <baalajimaestro@ptr.moe>
2026-08-30 19:14:59 +0530

    Update dependencies
    Signed-off-by: baalajimaestro <baalajimaestro@ptr.moe>

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, sync::Mutex, time::Duration};
 
-use ratelimit::Ratelimiter;
+use ratelimit::{Ratelimiter, TryWaitError};
 
 #[derive(Default)]
 pub(crate) struct Limiter {
@@ -9,17 +9,29 @@ pub(crate) struct Limiter {
 
 impl Limiter {
     pub(crate) fn wait(&self, user_id: i64) -> Result<(), Duration> {
-        self.inner
+        match self
+            .inner
             .lock()
             .unwrap()
             .entry(user_id)
             .or_insert_with(|| {
-                Ratelimiter::builder(30, Duration::from_secs(60))
+                Ratelimiter::builder(30)
+                    .period(Duration::from_secs(60))
                     .initial_available(30)
                     .max_tokens(120)
                     .build()
                     .expect("Ratelimiter with proper settings")
             })
             .try_wait()
+        {
+            Ok(()) => Ok(()),
+            Err(TryWaitError::Insufficient(dur)) => Err(dur),
+            Err(TryWaitError::ExceedsCapacity) => {
+                unreachable!("max_tokens=120, requesting 1 token")
+            }
+            Err(err) => {
+                unreachable!("unexpected TryWaitError variant: {err}")
+            }
+        }
     }
 }
